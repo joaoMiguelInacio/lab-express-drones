0b46492ecf5430124e1e5a888a6a8cbb9590b92d
joaoMiguelInacio <joao.miguel.inacio@gmail.com>
2022-05-05 22:21:34 +0100

    lab solved

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+npdocument.addEventListener('DOMContentLoaded', () => {
 
   console.log('lab-express-drones JS imported successfully!');
 
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,34 @@
-// Iteration #1
+const Drone = require("../models/drone.model.js");
+const mongoose = require("mongoose");
+require('../db');
+
+const drones = [
+    {
+    name: "DJI Air 2S",
+    propellers: 4,
+    maxSpeed: 68
+    }, 
+    {
+    name: "DJI Mini 2",
+    propellers: 4,
+    maxSpeed: 57
+    },
+    {
+    name: "DJI Mavic 3",
+    propellers: 4,
+    maxSpeed: 75
+    }
+];
+
+const droneSeed = async () => {
+    try{
+      await Drone.create(drones);
+      console.log(`${drones.length} drones created`);
+      await mongoose.connection.close();
+      console.log(`Disconnected from Mongo`);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  droneSeed();
